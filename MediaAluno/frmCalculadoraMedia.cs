f5b1c051bcc68b828ae05ed74a4048c62ff6d90f
ericksiqueiraa <erickpls06@gmail.com>
2022-09-17 19:24:13 -0300

    CALCULADORAMEDIA

---
+++ MediaAluno/frmCalculadoraMedia.cs
@@ -0,0 +1,15 @@
+namespace MediaAluno
+{
+    public partial class frmCalculadoraMedia : Form
+    {
+        public frmCalculadoraMedia()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
